03f32792b7f1f550561ba40a38747c8323e4d2be
Shonjoy Bhattacharyya <shonjoybhattacharyya@gmail.com>
2024-03-07 19:31:33 -0500

    added a few more courses to my courses page

--- byte-quest/Screens/HomeScreen.js
+++ byte-quest/Screens/HomeScreen.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { View, Text, Image, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import ExploreScreen from './ExploreScreen';
 import StackPic from '/Users/sb4sp/Desktop/Spring 24/CS 4261:8803/Byte-Quest/byte-quest/StackPic.png';
+import QueuePic from '/Users/sb4sp/Desktop/Spring 24/CS 4261:8803/Byte-Quest/byte-quest/QueuePic.png';
+import LinkedListPic from '/Users/sb4sp/Desktop/Spring 24/CS 4261:8803/Byte-Quest/byte-quest/LinkedListPic.png';
 
 const courses = [
   { id: '1', name: 'Stacks', image: StackPic},
-  
+  {id: '2', name: "Queues", image: QueuePic},
+  {id: '3', name: "Linked Lists", image: LinkedListPic }
   // Add more courses as needed
 ];
 
